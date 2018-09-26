ae74b0e850c71d41f34389b4665c313a42c15f7f
Georgi Stoyanov <georgi.stoyanov777@gmail.com>
2018-09-26 14:54:24 +0300

    Create database

---
+++ ModPanel/ModPanel.Data/ModPanelContext.cs
@@ -0,0 +1,42 @@
+﻿namespace ModPanel.Data
+{
+    using System.IO;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.Configuration;
+    using Models;
+
+    public class ModPanelContext : DbContext
+    {
+       public DbSet<User> Users { get; set; }
+
+        public DbSet<Post> Posts { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json")
+                    .Build();
+
+                var connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString);
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder
+                .Entity<User>()
+                .HasIndex(u => u.Email)
+                .IsUnique();
+
+            builder
+                .Entity<User>()
+                .HasMany(u => u.Posts)
+                .WithOne(p => p.User)
+                .HasForeignKey(u => u.UserId);
+        }
+    }
+}
---
+++ ModPanel/ModPanel/Startup.cs
@@ -0,0 +1,21 @@
+﻿namespace ModPanel
+{
+    using Data;
+    using SimpleMvc.Framework;
+    using SimpleMvc.Framework.Routers;
+    using WebServer;
+
+    public class Startup
+    {
+        public static void Main(string[] args)
+        {
+            var server = new WebServer(
+                7777,
+                DependencyControllerRouter.Get(),
+                new ResourceRouter());
+
+            var context = new ModPanelContext();
+            MvcEngine.Run(server, context);
+        }
+    }
+}
